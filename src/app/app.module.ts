0828b4c0ba6fca62db6bbe20619bbd69ab1f3d8e
Cookie Solomon <shoshsolomon0@gmail.com>
2018-02-22 10:34:35 +0200

    commit2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { AllCustomersComponent } from './all-customers/all-customers.component';
+import { AllCompaniesComponent } from './all-companies/all-companies.component';
+import { CompanyComponent } from './company/company.component';
+import { CompanyEditorComponent } from './company-editor/company-editor.component';
+import { CustomerEditorComponent } from './customer-editor/customer-editor.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    AllCustomersComponent,
+    AllCompaniesComponent,
+    CompanyComponent,
+    CompanyEditorComponent,
+    CustomerEditorComponent
   ],
   imports: [
     BrowserModule
